a10e61b0bf7a2347b8d1db17b2ae56d696f00e84
umarez <umar.izzuddin@ui.ac.id>
2021-05-09 02:06:33 +0700

    delete previous file

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -18,176 +18,211 @@ import {
   VStack,
   Textarea,
   Button,
+  Image,
+  useMediaQuery,
 } from "@chakra-ui/react";
-import { createBreakpoints } from "@chakra-ui/theme-tools";
-import Image from "next/image";
-import { FaPhoneAlt } from "react-icons/fa";
-import { IoChevronForwardSharp, IoMail, IoPeopleSharp } from "react-icons/io5";
+import { useState, useEffect } from "react";
+import { FaPhoneAlt, FaFacebookSquare } from "react-icons/fa";
+import { IoLogoFacebook } from "react-icons/io";
+import {
+  IoChevronForwardSharp,
+  IoMail,
+  IoPeopleSharp,
+  IoLocation,
+  IoLogoTwitter,
+  IoLogoInstagram,
+  IoLogoLinkedin,
+} from "react-icons/io5";
 
-const breakpoints = createBreakpoints({
-  xs: "20em", // 320px
-  sm: "30em", // 480px
-  md: "48em", // 768px
-  lg: "62em", // 992px
-  xl: "80em", // 1280px
-  "2xl": "96em", // 1536px
-});
-
-const contactUs = () => {
-  const theme = extendTheme({ breakpoints });
-  const style = {
-    transform: "translateY(-2rem)",
-  };
+const contact = () => {
+  const [isLargerThan768] = useMediaQuery("(min-width: 768px)");
+  const [isLargerThan1024] = useMediaQuery("(min-width: 1024px)");
+  const [isLargerThan1280] = useMediaQuery("(min-width: 1280px)");
 
   return (
-    <Center>
-      <Stack
-        width={["85%"]}
-        direction={["column"]}
-        pt="3rem"
-        // mx={["1rem", "2rem", "2rem", "3rem", "7rem"]}
-      >
-        <Box w="100%" pl="2rem">
-          <Breadcrumb separator={<IoChevronForwardSharp color="gray.500" />}>
-            <BreadcrumbItem>
-              <BreadcrumbLink href="#">Home</BreadcrumbLink>
-            </BreadcrumbItem>
+    <Flex
+      fontWeight="500"
+      mx={["2rem", "3rem", "2rem", "2rem", "5rem", "7.5rem"]}
+      flexDirection="column"
+      justifyContent="center"
+    >
+      <Box mt="1.25rem">
+        <Breadcrumb separator={<IoChevronForwardSharp size="0.75rem" />}>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="#">Home</BreadcrumbLink>
+          </BreadcrumbItem>
 
-            <BreadcrumbItem>
-              <BreadcrumbLink href="#" color="orange.400">
-                <b>Kontak Kami</b>
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-          </Breadcrumb>
-        </Box>
-        <Box>
-          <Text
-            mt="1rem"
-            mb="1rem"
-            fontSize={["4xl", "4xl", "5xl"]}
-            fontWeight="700"
+          <BreadcrumbItem isCurrentPage>
+            <BreadcrumbLink href="#" color="orange.400">
+              Kontak Kami
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        </Breadcrumb>
+
+        <Text as="h1" fontWeight="700" fontSize="2.25rem" mt="2rem">
+          Kontak Kami
+        </Text>
+
+        <Flex w="100%" flexDirection={isLargerThan768 ? "row" : "column"}>
+          <Box
+            w="22rem"
+            maxW={{ sm: "19.5rem", lg: "19.5rem", xl: "22.5rem" }}
+            mr="1rem"
           >
-            Kontak Kami
-          </Text>
-        </Box>
-        <Grid
-          mt="0 !important"
-          templateRows="2rem 10rem 10rem 4.5rem "
-          templateColumns="auto 28.4rem auto"
-          gap="1rem"
-        >
-          <GridItem rowSpan={1} order={2}>
+            <Flex w="100%" flexDirection="column">
+              <AspectRatio ratio={1 / 1} borderRadius="20px" overflow="hidden">
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.3005748285827!2d106.83741871529621!3d-6.355123363942694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69edea57fe727d%3A0xd91affb117d8be88!2sSabilaMall!5e0!3m2!1sen!2sid!4v1620499207093!5m2!1sen!2sid"
+                  width="22rem"
+                  height="22rem"
+                  allowfullscreen={true}
+                  loading="lazy"
+                ></iframe>
+              </AspectRatio>
+
+              <Flex mt="1rem">
+                <Box pr="1rem">
+                  <IoLocation size="3rem" color="orange" />
+                </Box>
+                <Flex direction="column">
+                  <Box>
+                    <Text as="h1" fontSize="0.875rem" fontWeight="500">
+                      Satria Building 2nd Lt 2 Unit A204-A206, Jl. Akses UI
+                      No.24-26, Tugu, Kec. Cimanggis, Kota Depok, Jawa Barat
+                      Depok Jawa Barat, 16451 Indonesia
+                    </Text>
+                  </Box>
+                  <Box mt="1rem">
+                    <Button
+                      variant="outline"
+                      fontWeight="700"
+                      colorScheme="orange"
+                      fontSize="0.875rem"
+                    >
+                      Lihat di Google Maps
+                    </Button>
+                  </Box>
+                </Flex>
+              </Flex>
+            </Flex>
+          </Box>
+
+          <Flex w="50%" direction="column" px="1rem">
             <Flex
-              h="10%"
-              order={[1, 1, 2]}
-              direction={["column", "column", "row"]}
+              h="20%"
+              w="100%"
+              direction="column"
+              justifyContent="space-between"
             >
-              <Stack
-                direction="row"
+              <Flex
+                w="100%"
+                h="50%"
                 alignItems="center"
-                height="3rem"
-                width="100%"
                 justifyContent="space-between"
               >
-                <Stack direction="row" alignItems="center">
-                  <FaPhoneAlt size="2rem" color="orange" />
+                <Flex alignItems="center">
+                  {!isLargerThan1280 ? (
+                    <FaPhoneAlt color="rgba(246, 173, 85, 1)" size="1rem" />
+                  ) : (
+                    <FaPhoneAlt color="rgba(246, 173, 85, 1)" size="2rem" />
+                  )}
+
                   <Text
-                    ml="0.64rem"
+                    fontSize={isLargerThan1280 ? "1rem" : "0.9rem"}
                     as="h1"
-                    fontSize="0.875rem"
-                    fontWeight="500"
+                    pl="0.64rem"
                   >
                     +62 851-5995-4161
                   </Text>
-                </Stack>
+                </Flex>
                 <Divider orientation="vertical" />
-                <Stack direction="row" alignItems="center">
-                  <IoMail size="2rem" color="orange" />
-                  <Text
-                    ml="0.64rem"
-                    as="h1"
-                    fontSize="0.875rem"
-                    fontWeight="500"
-                  >
-                    customer@sabilamall.co.id
-                  </Text>
-                </Stack>
-                <Divider orientation="Horizontal" />
-              </Stack>
+                <Flex alignItems="center">
+                  {!isLargerThan1280 ? (
+                    <IoMail color="rgba(246, 173, 85, 1)" size="1rem" />
+                  ) : (
+                    <IoMail color="rgba(246, 173, 85, 1)" size="2rem" />
+                  )}
+                  <Text pl="0.64rem">customer@sabilamall.co.id</Text>
+                </Flex>
+              </Flex>
+              {isLargerThan768 ? (
+                <Box h="31%">
+                  <Divider orientation="horizontal" />
+                </Box>
+              ) : null}
             </Flex>
-          </GridItem>
-          <GridItem rowSpan={3} order={1}>
-            <AspectRatio
-              w="22.5rem"
-              h="22.5rem"
-              ratio={1 / 1}
-              order={[2, 2, 1]}
-            >
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.3005748285827!2d106.83741871529621!3d-6.355123363942694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69edea57fe727d%3A0xd91affb117d8be88!2sSabilaMall!5e0!3m2!1sen!2sid!4v1620227743526!5m2!1sen!2sid"></iframe>
-            </AspectRatio>
-          </GridItem>
 
-          <GridItem rowSpan={4} order={3}>
-            <Flex position="relative" justifyContent="center">
-              {/* <Image
-                src="/Assets/sm-mascott.svg"
-                alt="sm-mascott"
-                layout="fill"
-                quality={100}
-              /> */}
-              <img style={style} src="/Assets/sm-mascott.svg" alt="" />
-            </Flex>
-          </GridItem>
-          <GridItem rowSpan={3} order={4} mt="2rem">
-            <Stack w="100%" h="100%">
-              <Text as="h1" fontSize={["2xl"]}>
-                <b>Tinggalkan Pesan</b>
+            <Box h="80%">
+              <Text as="h1" fontSize="1.5rem">
+                Tinggalkan Pesan
               </Text>
-              <VStack h="100%" spacing="1rem">
-                <InputGroup size="md" flexGrow="1">
+              <Stack spacing={4} mt="1.4rem">
+                <InputGroup>
                   <InputLeftElement
                     pointerEvents="none"
-                    children={<IoPeopleSharp size="1.4rem" color="#A0AEC0" />}
+                    children={<IoPeopleSharp color="#A0AEC0" />}
                   />
-                  <Input type="tel" placeholder="Nama Anda" size="md" />
+                  <Input type="tel" placeholder="Nama Anda" />
                 </InputGroup>
-                <InputGroup size="md" flexGrow="1">
+
+                <InputGroup>
                   <InputLeftElement
                     pointerEvents="none"
-                    children={<IoMail size="1.4rem" color="#A0AEC0" />}
+                    fontSize="1em"
+                    children={<IoMail color="#A0AEC0" />}
                   />
-                  <Input placeholder="Alamat email Anda" size="md" />
+                  <Input placeholder="Alamat email Anda" />
                 </InputGroup>
-                <InputGroup size="md" flexGrow="1">
+
+                <InputGroup>
                   <InputLeftElement
                     pointerEvents="none"
-                    children={<FaPhoneAlt size="1.4rem" color="#A0AEC0" />}
+                    fontSize="1em"
+                    children={<FaPhoneAlt color="#A0AEC0" />}
                   />
-                  <Input type="tel" placeholder="Nomor Telepon" />
+                  <Input placeholder="Nomor telepon Anda" />
                 </InputGroup>
                 <Textarea
-                  placeHolder="Tinggalkan pesan Anda di sini"
-                  size="md"
-                  w="100%"
-                  h="40%"
+                  h="7.5rem"
+                  placeholder="Tuliskan pesan Anda di sini"
+                  resize={"none"}
                 />
-                <Flex w="100%" justifyContent="flex-end">
+                <Flex justifyContent="flex-end">
                   <Button
+                    w="10.4rem"
                     colorScheme="red"
-                    variant="solid"
-                    size="md"
-                    width="10.4rem"
+                    color="white"
+                    fontWeight="700"
                   >
                     Kirim
                   </Button>
                 </Flex>
-              </VStack>
+              </Stack>
+            </Box>
+          </Flex>
+
+          <Flex direction="column">
+            <Box>
+              <Image
+                maxW={{ xl: "22rem" }}
+                src="/Assets/sm-mascott.svg"
+                alt=""
+              />
+            </Box>
+            <Text fontSize="1.5rem" fontWeight="700">
+              Follow SabilaMall yuk!
+            </Text>
+            <Stack direction="row" spacing={4} justifyContent="center">
+              <IoLogoFacebook size="2.5rem" color="orange" />
+              <IoLogoTwitter size="2.5rem" color="orange" />
+              <IoLogoInstagram size="2.5rem" color="orange" />
+              <IoLogoLinkedin size="2.5rem" color="orange" />
             </Stack>
-          </GridItem>
-        </Grid>
-      </Stack>
-    </Center>
+          </Flex>
+        </Flex>
+      </Box>
+    </Flex>
   );
 };
 
-export default contactUs;
+export default contact;
