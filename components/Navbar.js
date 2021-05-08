17528ce94ba0f19f720f3bb4b786f686d7a34936
Taufik Pragusga <pragusga25@gmail.com>
2021-05-09 06:56:13 +0700

    Fixed some problems [Revision 1]

--- components/Navbar.js
+++ components/Navbar.js
@@ -20,108 +20,28 @@ import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 import { FaUser } from "react-icons/fa";
 import {
   IoMenu,
-  IoCart,
   IoSearch,
-  IoHomeSharp,
   IoHeartSharp,
-  IoReceiptSharp,
   IoNotifications,
 } from "react-icons/io5";
 
+import { menuCategory, menuSidebar, icons } from "../constants/navbarConstant";
 import styles from "../styles/Navbar.module.scss";
 
-const styledIcon = {
-  color: "gray.500",
-  w: "23px",
-  h: "23px",
-  _hover: {
-    cursor: "pointer",
-  },
-};
-
-const fontSizeSidebar = {
-  "@media only screen and (max-width:300px)": {
-    fontSize: ".8em",
-  },
-  fontSize: ".9em",
-};
-
-const styledSidebarMenu = {
-  position: "fixed",
-  boxSizing: "border-box",
-  h: "100vh",
-  bg: "white",
-  fontWeight: "500",
-  lineHeight: "24px",
-  color: "black",
-  overflowX: "hidden",
-  overflowY: "auto",
-  transition: "width 0.4s",
-};
-
-const styledNavbar = {
-  position: "fixed",
-  bg: "white",
-  minWidth: "100vw",
-  boxSizing: "border-box",
-  overflow: "hidden",
-  alignItems: "center",
-};
-
-const BoxMenu = ({ children }) => (
+const NavbarBottom = () => (
   <Box
-    h="40px"
-    borderBottomWidth="1px"
-    borderColor="gray.200"
-    w="70vw"
-    display="flex"
-    alignItems="center"
-    justifyContent="space-between"
+    className={styles.navbarBottom}
+    zIndex="1"
+    display={{ base: "flex", md: "none" }}
   >
-    {children}
+    {icons.map((icon) => (
+      <Box className={styles.boxIcon} key={icon.id}>
+        <Icon as={icon.iconElement} className={styles.navbarIcon} />
+        <Text className={styles.boxIconText}>{icon.text}</Text>
+      </Box>
+    ))}
   </Box>
 );
-
-const NavbarBottom = () => {
-  const styledBox = {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-  };
-
-  const styledFont = {
-    fontSize: "12px",
-    color: "gray.500",
-    lineHeight: "18px",
-    fontWeight: "500",
-  };
-
-  const icons = [
-    { text: "Home", iconElement: IoHomeSharp, id: "ic1" },
-    { text: "Keranjang", iconElement: IoCart, id: "ic2" },
-    { text: "Pesanan", iconElement: IoReceiptSharp, id: "ic3" },
-    { text: "Akun", iconElement: FaUser, id: "ic4" },
-  ];
-
-  return (
-    <Box
-      sx={styledNavbar}
-      h="60px"
-      bottom="0"
-      border="1.5px solid #E2E8F0"
-      justifyContent="space-evenly"
-      display={{ base: "flex", md: "none" }}
-    >
-      {icons.map((icon) => (
-        <Box sx={styledBox} key={icon.id}>
-          <Icon as={icon.iconElement} sx={styledIcon} />
-          <Text sx={styledFont}>{icon.text}</Text>
-        </Box>
-      ))}
-    </Box>
-  );
-};
 
 const Navbar = () => {
   const [isSearched, setIsSearched] = useState(false);
@@ -133,151 +53,22 @@ const Navbar = () => {
     setIsCategoryMenu(false);
   };
 
-  const menuSidebar = {
-    headerText: "Menu",
-    menu: [
-      { text: "Kategori - Brand", id: "kb", href: "#" },
-      { text: "Mengenal SabilaMall", id: "ms", href: "#" },
-      { text: "Gabung Reseller Baju Murah", id: "grbm", href: "#" },
-      { text: "Kebijakan Privasi", id: "kp", href: "#" },
-      { text: "Syarat & Ketentuan", id: "sk", href: "#" },
-      { text: "Hubungi Kami", id: "hk", href: "#" },
-      { text: "Download Aplikasi", id: "dagp", href: "#" },
-    ],
-  };
-
-  const menuCategory = {
-    headerText: "Kategori - Brand",
-    menu: [
-      {
-        text: "Aksesoris Fashion",
-        id: "kjqsb13",
-        subMenu: [
-          { text: "Kaus Muslim Keren Banget", href: "#", id: "dancxal12" },
-          { text: "Kaus Kaki Muslimah", href: "#", id: "cnshqwm2" },
-        ],
-      },
-      {
-        text: "Buku & Alat Tulis",
-        id: "mlk1871",
-        subMenu: [
-          { text: "Kaus Muslim Keren Banget", href: "#", id: "mdjayqbd" },
-          { text: "Kaus Kaki Muslimah", href: "#", id: "kadindjqb1" },
-        ],
-      },
-      {
-        text: "Fashion Bayi & Anak",
-        id: "mllkpo98",
-        subMenu: [
-          { text: "Kaus Muslim Keren Banget", href: "#", id: "ncnciwm2" },
-          { text: "Kaus Kaki Muslimah", href: "#", id: "nclal2298" },
-        ],
-      },
-      {
-        text: "Fashion Muslim",
-        id: "lkqn131",
-        subMenu: [
-          { text: "Kaus Muslim Keren Banget", href: "#", id: "clleu27y28u" },
-          { text: "Kaus Kaki Muslimah", href: "#", id: "nclaei2ei" },
-        ],
-      },
-      {
-        text: "Kesehatan",
-        id: "mkoqi187",
-        subMenu: [
-          { text: "Kaus Muslim Keren Banget", href: "#", id: "mce2e87d" },
-          { text: "Kaus Kaki Muslimah", href: "#", id: "ncswiud28" },
-        ],
-      },
-      {
-        text: "Mainan",
-        id: "mlkqoi12",
-        subMenu: [
-          { text: "Kaus Muslim Keren Banget", href: "#", id: "bncwiue28" },
-          { text: "Kaus Kaki Muslimah", href: "#", id: "oi02832h" },
-        ],
-      },
-      {
-        text: "Makanan dan Minuman",
-        id: "lklo910j",
-        subMenu: [
-          { text: "Kaus Muslim Keren Banget", href: "#", id: "bii2392" },
-          { text: "Kaus Kaki Muslimah", href: "#", id: "i1u98hsq" },
-        ],
-      },
-      {
-        text: "Perawatan & Kecantikan",
-        id: "mxlkw12q1",
-        subMenu: [
-          { text: "Kaus Muslim Keren Banget", href: "#", id: "nckwie28" },
-          { text: "Kaus Kaki Muslimah", href: "#", id: "mlqo091" },
-        ],
-      },
-      {
-        text: "Perlengkapan Dapur",
-        id: "lkpo28u",
-        subMenu: [
-          { text: "Kaus Muslim Keren Banget", href: "#", id: "nodqwi92" },
-          { text: "Kaus Kaki Muslimah", href: "#", id: "oi2e89j" },
-        ],
-      },
-      {
-        text: "Perlengkapan Ibadah",
-        id: " lkw52",
-        subMenu: [
-          { text: "Kaus Muslim Keren Banget", href: "#", id: "kiqii92" },
-          { text: "Kaus Kaki Muslimah", href: "#", id: "oi2928j" },
-        ],
-      },
-      {
-        text: "Perlengkapan Rumah Tangga",
-        id: "mxlla981",
-        subMenu: [
-          { text: "Kaus Muslim Keren Banget", href: "#", id: "oo029e" },
-          { text: "Kaus Kaki Muslimah", href: "#", id: "lko0e29u" },
-        ],
-      },
-      {
-        text: "Sepatu & Tas",
-        id: "mdaljq121",
-        subMenu: [
-          { text: "Kaus Muslim Keren Banget", href: "#", id: "po2e3209" },
-          { text: "Kaus Kaki Muslimah", href: "#", id: "oi20e92" },
-        ],
-      },
-      {
-        text: "Serba-serbi",
-        id: "pnsqj12",
-        subMenu: [
-          { text: "Ciput", href: "#", id: "cpad123" },
-          { text: "Kaus Kaki Muslimah", href: "#", id: "kalsa123" },
-        ],
-      },
-    ],
-  };
-
   return (
     <>
       <Box
-        sx={styledNavbar}
-        justifyContent="space-between"
-        top="0"
-        position="fixed"
-        boxShadow="0px 4px 10px 0px #00000040"
-        h={{ base: "50px", md: "70px" }}
-        display="flex"
+        className={styles.navbarTop}
         px={{ md: "40px", lg: "80px", xl: "120px" }}
-        className={styles.navbarFont}
-        zIndex={isSearched ? "107" : ""}
+        h={{ base: "50px", md: "70px" }}
+        zIndex={isSearched ? "5" : ""}
       >
         <Box
           display="flex"
-          ml={{ base: "15px", md: "20px" }}
           alignItems="center"
+          ml={{ base: "15px", md: "20px" }}
         >
           <Icon
             as={IoMenu}
-            sx={styledIcon}
+            className={styles.navbarIcon}
             display={{ base: "block", md: "none" }}
             onClick={() => setIsMainMenu(true)}
           />
@@ -314,35 +105,30 @@ const Navbar = () => {
               borderWidth="0"
               bg="gray.100"
               pl="55px"
+              focusBorderColor="gray.100"
             />
           </InputGroup>
         </Box>
         <Box display="flex" alignItems="center" mr="20px">
           <Icon
             as={IoHeartSharp}
-            sx={styledIcon}
+            className={styles.navbarIcon}
             mr={{ base: "8px", md: "20px", lg: "25px" }}
           />
-          <Icon as={IoNotifications} sx={styledIcon} />
+          <Icon as={IoNotifications} className={styles.navbarIcon} />
           <Icon
             as={FaUser}
-            sx={styledIcon}
+            className={styles.navbarIcon}
             ml={{ base: "8px", md: "20px", lg: "25px" }}
             display={{ base: "none", md: "block" }}
           />
         </Box>
         <Box
-          position="absolute"
-          zIndex={isSearched ? "110" : "-100"}
-          h="100%"
-          right="0"
-          top="0"
+          className={styles.boxSearch}
+          display={{ base: "flex", md: "none" }}
+          zIndex={isSearched ? "6" : "-6"}
           w={isSearched ? "100vw" : "128px"}
           bg={isSearched ? "white" : "transparent"}
-          display={{ base: "flex", md: "none" }}
-          alignItems="center"
-          px="20px"
-          transition="width 0.8s, background-color 0.8s"
         >
           <Icon
             as={BsChevronLeft}
@@ -353,15 +139,12 @@ const Navbar = () => {
             _hover={{ cursor: "pointer" }}
             display={isSearched ? "block" : "none"}
           />
-          <InputGroup
-            borderColor="white"
-            isDisabled={isSearched ? "false" : "true"}
-          >
+          <InputGroup borderColor="white">
             <InputLeftElement
               children={
                 <Icon
                   as={IoSearch}
-                  sx={styledIcon}
+                  className={styles.navbarIcon}
                   onClick={() => setIsSearched(true)}
                   mr="20px"
                 />
@@ -373,11 +156,11 @@ const Navbar = () => {
               type="text"
               placeholder="Cari di toko..."
               bg={isSearched ? "gray.100" : "transparent"}
-              borderRadius="12px"
-              borderWidth="0"
               pl={isSearched ? "45px" : "0"}
               w={isSearched ? "100%" : "0"}
               visibility={isSearched ? "visible" : "hidden"}
+              borderRadius="12px"
+              borderWidth="0"
               transition="width 0.8s, padding-left 0.8s,  background-color 0s, visibility 0s"
             />
           </InputGroup>
@@ -385,23 +168,19 @@ const Navbar = () => {
       </Box>
       <Box
         position="fixed"
-        zIndex="101"
+        zIndex="2"
         bg="gray.500"
         opacity="50%"
         w={isMainMenu || isSearched ? "100vw" : "0"}
         h="100vh"
         onClick={handleClickOverlay}
       ></Box>
-      <Box w={isMainMenu ? "85vw" : "0"} zIndex="102" sx={styledSidebarMenu}>
-        <Box
-          w="100%"
-          h="50px"
-          boxShadow="0px 4px 10px 0px #00000040"
-          boxSizing="border-box"
-          bg="white"
-          display="flex"
-          alignItems="center"
-        >
+      <Box
+        w={isMainMenu ? "85vw" : "0"}
+        zIndex="3"
+        className={styles.sidebarMenu}
+      >
+        <Box className={styles.headerSidebar}>
           <Icon
             as={BsChevronLeft}
             onClick={
@@ -415,7 +194,7 @@ const Navbar = () => {
             h="20px"
           />
           <Heading
-            sx={fontSizeSidebar}
+            className={styles.fontSizeSidebar}
             visibility={isCategoryMenu ? "hidden" : "visible"}
           >
             {menuSidebar.headerText}
@@ -426,17 +205,11 @@ const Navbar = () => {
             if (item.id === "kb") {
               return (
                 <Box
-                  h="40px"
-                  borderBottomWidth="1px"
-                  borderColor="gray.200"
-                  w="70vw"
-                  display="flex"
-                  alignItems="center"
-                  justifyContent="space-between"
+                  className={styles.boxMenu}
                   key={item.id}
                   onClick={() => setIsCategoryMenu(true)}
                 >
-                  <Heading sx={fontSizeSidebar} lineHeight="50px">
+                  <Heading className={styles.fontSizeSidebar} lineHeight="50px">
                     {item.text}
                   </Heading>
                   <Icon as={BsChevronRight} w="20px" h="20px" />
@@ -451,7 +224,10 @@ const Navbar = () => {
                   key={item.id}
                 >
                   <Link href={item.href}>
-                    <Heading sx={fontSizeSidebar} lineHeight="40px">
+                    <Heading
+                      className={styles.fontSizeSidebar}
+                      lineHeight="40px"
+                    >
                       {item.text}
                     </Heading>
                   </Link>
@@ -465,33 +241,25 @@ const Navbar = () => {
               );
             }
             return (
-              <BoxMenu key={item.id}>
+              <Box className={styles.boxMenu} key={item.id}>
                 <Link href={item.href}>
-                  <Heading sx={fontSizeSidebar} lineHeight="50px">
+                  <Heading className={styles.fontSizeSidebar} lineHeight="50px">
                     {item.text}
                   </Heading>
                 </Link>
-              </BoxMenu>
+              </Box>
             );
           })}
         </VStack>
       </Box>
       <Box
         w={isCategoryMenu ? "85vw" : "0"}
-        zIndex="103"
-        sx={styledSidebarMenu}
+        className={styles.sidebarMenu}
+        zIndex="4"
         overflow="hidden"
         transition="width .3s"
       >
-        <Box
-          w="100%"
-          h="50px"
-          boxShadow="0px 4px 10px 0px #00000040"
-          boxSizing="border-box"
-          bg="white"
-          display="flex"
-          alignItems="center"
-        >
+        <Box className={styles.headerSidebar}>
           <Icon
             as={BsChevronLeft}
             onClick={
@@ -505,7 +273,7 @@ const Navbar = () => {
             h="20px"
           />
           <Heading
-            sx={fontSizeSidebar}
+            className={styles.fontSizeSidebar}
             visibility={isCategoryMenu ? "visible" : "hidden"}
           >
             {menuCategory.headerText}
@@ -517,14 +285,11 @@ const Navbar = () => {
               return (
                 <AccordionItem key={item.id}>
                   <AccordionButton borderWidth="0" borderColor="transparent">
-                    <Box
-                      h="40px"
-                      w="70vw"
-                      display="flex"
-                      alignItems="center"
-                      justifyContent="space-between"
-                    >
-                      <Heading sx={fontSizeSidebar} lineHeight="30px">
+                    <Box className={styles.boxCategoryMenu}>
+                      <Heading
+                        className={styles.fontSizeSidebar}
+                        lineHeight="30px"
+                      >
                         {item.text}
                       </Heading>
                       <AccordionIcon />
@@ -533,7 +298,7 @@ const Navbar = () => {
                   <AccordionPanel pb={1} pl="30px">
                     {item.subMenu.map((sub) => (
                       <Heading
-                        sx={fontSizeSidebar}
+                        className={styles.fontSizeSidebar}
                         lineHeight="40px"
                         key={sub.id}
                       >
