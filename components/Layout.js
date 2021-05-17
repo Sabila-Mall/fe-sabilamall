318681172b7a122228e433d94c622fa53b487219
umarez <umar.izzuddin@ui.ac.id>
2021-05-17 21:27:09 +0700

    implement layout

--- components/Layout.js
+++ components/Layout.js
@@ -2,9 +2,10 @@ import { Box } from "@chakra-ui/react";
 
 import styles from "../styles/Layout.module.scss";
 import { BreadCrumb } from "./Breadcrumb";
+import Footer from "./Footer";
 import Navbar from "./Navbar";
 
-// Untuk props berupa hasNavbar & hasBreadCrumb -> Punya navbar, punya breadcrumb?
+// Untuk props berupa hasNavbar, hasBreadCrumb, hasFooter -> Punya navbar, punya breadcrumb?
 // Untuk passing breadcrumb berupa array of object, defaultnya udah ada home, jadi kalo nambahin bakal jadi Home > ... > dst
 // contoh passing props breadcrumb: breadCrumbItem = [{name: "Kontak Kami", link: "https://google.com", isOnPage}]
 // isOnPage untuk jadi penanda  active page di breadCrumbnya
@@ -14,6 +15,7 @@ export const Layout = ({
   children,
   hasBreadCrumb,
   breadCrumbItem,
+  hasFooter,
 }) => {
   return (
     <>
@@ -28,6 +30,7 @@ export const Layout = ({
           {children}
         </Box>
       </Box>
+      {hasFooter && <Footer />}
     </>
   );
 };
