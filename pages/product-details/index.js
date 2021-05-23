e346d3aa1b8eff873ac4f2c8ada90b761b3314ea
Teofanus Gary Setiawan <garyteofanus@gmail.com>
2021-05-24 04:30:07 +0700

    Partial implementation of product review
    Implemented comment block (except profile image) and title

--- pages/product-details/index.js
+++ pages/product-details/index.js
@@ -1,14 +1,19 @@
-import { Box } from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react";
 
-import ProductInformation from '../../components/ProductInformation';
-import ProductHeader from '../../components/ProductHeader';
+import ProductInformation from "../../components/ProductInformation";
+import ProductHeader from "../../components/ProductHeader";
+import ProductReview from "../../components/ProductReview";
+
 const ProductDetails = () => {
-    return(
-        <Box w={{ base : "100%", md : "35%" }} ml={{base: "12px"}} mr={{base: "12px"}}>
-            <ProductHeader />
-            <ProductInformation />
-        </Box>
-    )
-}
+  return (
+    <>
+      <Box w={{ base: "100%", md: "35%" }} ml={{ base: "12px" }} mr={{ base: "12px" }}>
+        <ProductHeader />
+        <ProductInformation />
+      </Box>
+      <ProductReview />
+    </>
+  );
+};
 
-export default ProductDetails
+export default ProductDetails;
