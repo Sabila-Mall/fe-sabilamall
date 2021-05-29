3e26b2b54d4106706aa7015631fbdb8fe034f8c7
umarez <umar.izzuddin@ui.ac.id>
2021-05-30 00:59:58 +0700

    checkout card calculation

--- components/CardCheckout.js
+++ components/CardCheckout.js
@@ -1,31 +1,45 @@
 import { Button } from "@chakra-ui/button";
 import { Box, Divider, Flex, HStack, Text, VStack } from "@chakra-ui/layout";
+import { css } from "@emotion/react";
 
-export const CardCheckout = () => {
+export const CardCheckout = ({ subTotal, discount }) => {
+  const idr = Intl.NumberFormat("id-ID");
+  const subtotal = subTotal.replace(/\./g, "");
+  const disc = discount.replace(/\./g, "");
+  const total = Number(subtotal) - Number(disc);
+  console.log(total);
+
   return (
     <Box
       width="full"
       borderRadius="12px"
       border="solid #CBD5E0 0.5px"
       px="1rem"
+      py="12px"
     >
       <Flex flexDirection="column" color="gray.500" fontWeight="500">
         <Flex my="6px" justifyContent="space-between">
           <Text fontWeight="700">Subtotal</Text>
-          <Text>Rp99.999.999</Text>
+          <Text>Rp{subTotal}</Text>
         </Flex>
         <Flex my="6px" justifyContent="space-between">
           <Text fontWeight="700">Diskon</Text>
-          <Text>Rp99.999.999</Text>
+          <Text>-Rp{discount}</Text>
         </Flex>
         <Divider my="6px" />
         <Flex my="6px" justifyContent="space-between">
           <Text fontWeight="700">Total</Text>
           <Text fontSize="1.2rem" color="orange.500">
-            Rp99.999.999
+            Rp{idr.format(total)}
           </Text>
         </Flex>
-        <Button my="3px" colorScheme="red" size="md">
+        <Button
+          my="3px"
+          colorScheme="red"
+          bgColor="red.500"
+          size="md"
+          color="white"
+        >
           Proses ke Checkout
         </Button>
       </Flex>
--- pages/cart-details/index.js
+++ pages/cart-details/index.js
@@ -4,8 +4,8 @@ import { CardCheckout } from "../../components/CardCheckout";
 
 const cartDetails = () => {
   return (
-    <Box w="269px" h="188px">
-      <CardCheckout />;
+    <Box w="269px">
+      <CardCheckout subTotal={"99.999.999"} discount={"89.999.999"} />;
     </Box>
   );
 };
