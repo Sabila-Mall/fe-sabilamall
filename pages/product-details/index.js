b453e08de0d8a0ddcbf4a85301317f26c6c476e2
umarez <umar.izzuddin@ui.ac.id>
2021-05-29 17:22:44 +0700

    init card for product details

--- pages/product-details/index.js
+++ pages/product-details/index.js
@@ -1,17 +1,19 @@
 import { Box } from "@chakra-ui/react";
 
-import ProductInformation from "../../components/ProductInformation";
 import ProductHeader from "../../components/ProductHeader";
+import ProductInformation from "../../components/ProductInformation";
 import ProductReview from "../../components/ProductReview";
+import { CardProduct } from "./card";
 
 const ProductDetails = () => {
   return (
     <>
-      <Box w={{ base: "100%", md: "35%" }} ml={{ base: "12px" }} mr={{ base: "12px" }}>
+      {/* <Box w={{ base: "100%", md: "35%" }} ml={{ base: "12px" }} mr={{ base: "12px" }}>
         <ProductHeader />
         <ProductInformation />
       </Box>
-      <ProductReview />
+      <ProductReview /> */}
+      <CardProduct isDiscount={true} discountAmount={"10%"} />
     </>
   );
 };
