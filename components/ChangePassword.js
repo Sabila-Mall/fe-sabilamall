19f4a9219a227dbaae72dcef39f5ef76edaf922a
Taufik Pragusga <pragusga25@gmail.com>
2021-07-05 17:28:28 +0700

    Change button position

--- components/ChangePassword.js
+++ components/ChangePassword.js
@@ -12,63 +12,75 @@ export const ChangePassword = ({ isMobile }) => {
 
   return (
     <>
-      <Box mt={{ base: "30px", md: "10px" }} px="16px" pt="18px">
-        <FormControl as="form" onSubmit={handleSubmit(onSubmit)}>
-          <Box px="12px">
-            <InputBoxAndLabel
-              register={register}
-              text="Password Lama"
-              name={isMobile ? "oldPasswordMobile" : "oldPassword"}
-              mt="2px"
-              type="password"
-            />
-            <InputBoxAndLabel
-              register={register}
-              text="Password Baru"
-              name={isMobile ? "newPasswordMobile" : "newPassword"}
-              mt={{ base: "50px", md: "25px" }}
-              type="password"
-            />
-            <InputBoxAndLabel
-              register={register}
-              text="Konfirmasi Password Baru"
-              name={
-                isMobile ? "newPasswordConfirmMobile" : "newPasswordConfirm"
-              }
-              mt="20px"
-              type="password"
-            />
-          </Box>
+      {/* <Box
+        mt={{ base: "30px", md: "10px" }}
+        px="16px"
+        pt="18px"
+        h="100vh"
+        bg="red"
+      > */}
+      <FormControl
+        as="form"
+        // px="16px"
+        // pt="18px"
+        pt={{ base: "48px", md: "28px" }}
+        onSubmit={handleSubmit(onSubmit)}
+        h={isMobile && "100vh"}
+        position="relative"
+      >
+        <Box px="28px">
+          <InputBoxAndLabel
+            register={register}
+            text="Password Lama"
+            name={isMobile ? "oldPasswordMobile" : "oldPassword"}
+            mt="2px"
+            type="password"
+          />
+          <InputBoxAndLabel
+            register={register}
+            text="Password Baru"
+            name={isMobile ? "newPasswordMobile" : "newPassword"}
+            mt={{ base: "50px", md: "25px" }}
+            type="password"
+          />
+          <InputBoxAndLabel
+            register={register}
+            text="Konfirmasi Password Baru"
+            name={isMobile ? "newPasswordConfirmMobile" : "newPasswordConfirm"}
+            mt="20px"
+            type="password"
+          />
+        </Box>
+        <Box position="absolute" w="100%" px="28px" bottom={20}>
           <Button
             className="primaryFont"
+            type="submit"
             fontWeight="700"
             fontSize="14px"
             lineHeight="18.2px"
-            type="submit"
-            mx="auto"
             borderRadius="20px"
             w="100%"
             bg="orange.400"
             color="white"
             colorScheme="orange.400"
-            mt="2rem"
             _active={{ bg: "orange.300" }}
             border="none"
             display={{ base: "block", md: "none" }}
           >
             Ubah Password
           </Button>
-          <Flex justify="flex-end" w="100%" mt="70px">
-            <ButtonSubmit text="Ubah Password" />
-          </Flex>
-        </FormControl>
-      </Box>
+        </Box>
+        <Flex justify="flex-end" w="100%" mt="70px">
+          <ButtonSubmit text="Ubah Password" />
+        </Flex>
+      </FormControl>
+      {/* </Box> */}
     </>
   );
 };
 
 export const ChangePasswordMobile = () => (
-  <Box display={{ base: "block", md: "none" }}>
+  <Box display={{ base: "block", md: "none" }} h="100vh">
     <NavbarProfile section={"Ubah Password"} />
     <ChangePassword isMobile={true} />
   </Box>
