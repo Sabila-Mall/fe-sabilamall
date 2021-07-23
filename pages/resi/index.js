9216489569e0409d9a1a6aad20a67e4fd6cb8f7d
umarez <umar.izzuddin@ui.ac.id>
2021-07-23 14:32:39 +0700

    adding pesanan detail

---
+++ pages/resi/index.js
@@ -0,0 +1,69 @@
+const { Box, Text, Flex, Button, Divider } = require("@chakra-ui/react");
+const { Layout } = require("../../components/Layout");
+
+const resi = () => {
+  return (
+    <>
+      <Box
+        px="0.5rem"
+        w="full"
+        h="3.125rem"
+        display="flex"
+        alignItems="center"
+        boxShadow="0px 4px 10px 0px #00000040"
+      >
+        <Text fontWeight="700" color="gray.500">
+          Informasi Pesanan
+        </Text>
+      </Box>
+      <Layout>
+        <Box
+          my="-3rem"
+          boxShadow="0px 4px 10px 0px #00000040"
+          borderRadius="1rem"
+          p="0.75rem"
+        >
+          <Box fontWeight="500">
+            <TextWLabel title="Nomor Pesanan" desc="SMC10101" />
+            <Box maxW="280px">
+              <Flex justifyContent="space-between">
+                <TextWLabel title="Status Pesanan" desc="Terkirim" />
+                <TextWLabel title="Waktu Pemesanan" desc="10 Juli 2021 13:11" />
+              </Flex>
+              <Flex justifyContent="space-between" mb="1rem">
+                <TextWLabel title="Status Pembayaran" desc="Dikonfirmasi" />
+                <TextWLabel
+                  title="Waktu Pembayaran"
+                  desc="10 Juli 2021 13:11"
+                />
+              </Flex>
+            </Box>
+            <Flex flexWrap="wrap" mb="1rem">
+              <Button colorScheme="orange" w="full" mb="0.5rem">
+                Berikan Penilaian
+              </Button>
+              <Button w="full" colorScheme="orange" variant="outline">
+                Lihat Invoice
+              </Button>
+            </Flex>
+            <Divider w="full" />
+          </Box>
+        </Box>
+      </Layout>
+    </>
+  );
+};
+
+const TextWLabel = ({ title, desc }) => {
+  return (
+    <>
+      <Box mb="0.5rem">
+        <Text color="gray.500" fontSize="0.8rem">
+          {title}
+        </Text>
+        <Text color="black">{desc}</Text>
+      </Box>
+    </>
+  );
+};
+export default resi;
