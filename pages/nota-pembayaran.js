f9715b21c605dba1ae8fea187d5e1bd68900cdd7
Taufik Pragusga <pragusga25@gmail.com>
2021-07-26 23:39:36 +0700

    init

---
+++ pages/nota-pembayaran.js
@@ -0,0 +1,36 @@
+import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
+
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: "row",
+    backgroundColor: "#E4E4E4",
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1,
+  },
+});
+
+function NotaPembayaran() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  return (
+    <>
+      <Document file="somefile.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
+    </>
+  );
+}
+
+export default NotaPembayaran;
