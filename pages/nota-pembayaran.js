d259bae75ddb0c2228a6d1959ce812ae21424d5c
Taufik Pragusga <pragusga25@gmail.com>
2021-07-28 22:15:34 +0700

    Create nota-pembayaran

--- pages/nota-pembayaran.js
+++ pages/nota-pembayaran.js
@@ -1,36 +1,13 @@
-import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
+import { Box } from "@chakra-ui/react";
+import dynamic from "next/dynamic";
 
-// Create styles
-const styles = StyleSheet.create({
-  page: {
-    flexDirection: "row",
-    backgroundColor: "#E4E4E4",
-  },
-  section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1,
-  },
+const PDFViewer = dynamic(() => import("../components/PDFViewer"), {
+  ssr: false,
 });
-
-function NotaPembayaran() {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
-
+export default function PDF() {
   return (
-    <>
-      <Document file="somefile.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
-      </Document>
-      <p>
-        Page {pageNumber} of {numPages}
-      </p>
-    </>
+    <Box h="100%">
+      <PDFViewer />
+    </Box>
   );
 }
-
-export default NotaPembayaran;
