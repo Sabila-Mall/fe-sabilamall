67716afb70549d69894ac8a4ad479269be0e7c51
Umar Izzuddin <umar.izzuddin@ui.ac.id>
2021-08-01 22:24:26 +0700

    fixing share product color

--- components/ShareProduct.js
+++ components/ShareProduct.js
@@ -2,6 +2,25 @@ import { Box, Text, Flex, HStack, Link } from "@chakra-ui/react";
 import { IoLogoFacebook, IoMdLink } from "react-icons/io";
 import { IoLogoTwitter, IoLogoWhatsapp } from "react-icons/io5";
 
+const d = [
+  {
+    link: "https://google.com",
+    logo: <IoLogoFacebook size="1.5em" />,
+  },
+  {
+    link: "https://google.com",
+    logo: <IoLogoTwitter size="1.5em" />,
+  },
+  {
+    link: "https://google.com",
+    logo: <IoLogoWhatsapp size="1.5em" />,
+  },
+  {
+    link: "https://google.com",
+    logo: <IoMdLink size="1.5em" />,
+  },
+];
+
 export const ShareProduct = () => {
   return (
     <Box>
@@ -14,46 +33,23 @@ export const ShareProduct = () => {
         <Text color="black" fontWeight="semibold">
           Bagikan
         </Text>
-        <Link href="https://google.com" isExternal>
-          <Box
-            border="2px solid #E53E3E"
-            borderRadius="24px"
-            p="0.35rem"
-            cursor="pointer"
-          >
-            <IoLogoFacebook size="1.5em" />
-          </Box>
-        </Link>
-        <Link href="https://google.com" isExternal>
-          <Box
-            border="2px solid #E53E3E"
-            borderRadius="24px"
-            p="0.35rem"
-            cursor="pointer"
-          >
-            <IoLogoTwitter size="1.5em" />
-          </Box>
-        </Link>
-        <Link href="https://google.com" isExternal>
-          <Box
-            border="2px solid #E53E3E"
-            borderRadius="24px"
-            p="0.35rem"
-            cursor="pointer"
-          >
-            <IoLogoWhatsapp size="1.5em" />
-          </Box>
-        </Link>
-        <Link href="https://google.com" isExternal>
-          <Box
-            border="2px solid #E53E3E"
-            borderRadius="24px"
-            p="0.35rem"
-            cursor="pointer"
-          >
-            <IoMdLink size="1.5em" />
-          </Box>
-        </Link>
+        {d.map((e, i) => {
+          return (
+            <>
+              <Link href={e.link} isExternal>
+                <Box
+                  _hover={{ textColor: "white", background: "red" }}
+                  border="2px solid #E53E3E"
+                  borderRadius="24px"
+                  p="0.35rem"
+                  cursor="pointer"
+                >
+                  {e.logo}
+                </Box>
+              </Link>
+            </>
+          );
+        })}
       </HStack>
     </Box>
   );
