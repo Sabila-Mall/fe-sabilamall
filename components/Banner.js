78c3c6dc0ab39ac63ad8c0aebf1cd465e04b1ed4
ganiilhamirsyadi@gmail.com <ganiilhamirsyadi@gmail.com>
2021-09-16 20:22:43 +0700

    Fix banner

--- components/Banner.js
+++ components/Banner.js
@@ -1,9 +1,10 @@
 import { Box, Center, Img, Spinner } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import Slider from "react-slick";
 
 import { IMAGE_HOST } from "../constants/api";
+import { useWindowSize } from "../hooks/useWindowSize";
 
 const settings = {
   centerMode: true,
@@ -18,6 +19,8 @@ const settings = {
 
 const Banner = ({ data, loading }) => {
   const [display, setDisplay] = useState("none");
+  const { width } = useWindowSize();
+
   let ref = null;
 
   return loading ? (
@@ -52,22 +55,47 @@ const Banner = ({ data, loading }) => {
           <MdChevronLeft size="2em" />
         </Box>
       </Box>
-      <Box>
+      <Box
+        maxWidth="1440px"
+        width={width <= 576 ? window.innerWidth * 1.2 + "px" : "100%"}
+        position="relative"
+        left={width <= 576 ? -window.innerWidth * 0.1 + "px" : 0}
+      >
         <Slider
           ref={(node) => {
             ref = node;
           }}
           {...settings}
         >
-          {data.map((each) => (
-            <Img
-              className="imageRound"
-              src={IMAGE_HOST + each.image_path}
-              pl={{ base: "0.2rem", xl: "0.5rem" }}
-              pr={{ base: "0.2rem", xl: "0.5rem" }}
-              key={each.id}
-            />
-          ))}
+          {width <= 576
+            ? data.map((each) => (
+                <Box
+                  key={data?.id}
+                  display="flex !important"
+                  justifyContent="center"
+                >
+                  <Box
+                    boxSizing="border-box"
+                    width={0.9 * window.innerWidth + "px"}
+                    height={(0.9 / 3) * window.innerWidth + "px"}
+                    className="imageRound"
+                    ml={{ base: "0.25rem", xl: "0.5rem" }}
+                    mr={{ base: "0.25rem", xl: "0.5rem" }}
+                    backgroundImage={`url(${IMAGE_HOST + each.image_path})`}
+                    backgroundPosition="center"
+                    backgroundSize="cover"
+                  ></Box>
+                </Box>
+              ))
+            : data.map((each) => (
+                <Img
+                  className="imageRound"
+                  src={IMAGE_HOST + each.image_path}
+                  pl={{ base: "0.2rem", xl: "0.5rem" }}
+                  pr={{ base: "0.2rem", xl: "0.5rem" }}
+                  key={each.id}
+                />
+              ))}
         </Slider>
       </Box>
       <Box
