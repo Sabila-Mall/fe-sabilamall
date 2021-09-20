a440c71d4720d92c49fbb00386cc025190153612
Teofanus Gary Setiawan <40825899+garyteofanus@users.noreply.github.com>
2021-09-20 23:39:46 +0700

    Add userId to get products by category api (#169)

--- api/DaftarProduk.js
+++ api/DaftarProduk.js
@@ -1,9 +1,14 @@
-import { HOST } from "../constants/api";
+import {HOST} from "../constants/api";
 import axios from "axios";
-import { isRequestSuccess } from "../utils/api";
+import {isRequestSuccess} from "../utils/api";
 
-export const getProductsByCategory = async (categoryId, page = 1) => {
-  const res = await axios.get(HOST + `/api/category/get_all_products_by_category/${categoryId}?page=${page}`);
+export const getProductsByCategory = async (categoryId, page = 1, customerId) => {
+  const res = await axios.get(HOST + `/api/category/get_all_products_by_category/${categoryId}`, {
+    params: {
+      page: page,
+      customers_id: customerId,
+    }
+  });
 
   if (isRequestSuccess(res.data)) {
     return await res.data;
