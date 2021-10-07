4bb9616ba4d20480632d80315630d42ffdda8a39
Gani Ilham Irsyadi <ganiilhamirsyadi@gmail.com>
2021-10-07 13:30:54 +0700

    Wildanfuady (#200)
    * update warna status order
    
    * update warna status order
    
    * Extract the function
    
    * update tampil no resi di halaman list pesanan
    
    Co-authored-by: wildanfuady <wildanfuady@gmail.com>

--- pages/profile/pesanan-saya.js
+++ pages/profile/pesanan-saya.js
@@ -152,6 +152,7 @@ const CardPesanan = ({
   totalPrice,
   orderId,
   parentId,
+  shippingResi,
   totalPriceAgent,
 }) => {
   const router = useRouter();
@@ -259,13 +260,38 @@ const CardPesanan = ({
             {deliverer}
           </Text>
         </GridItem>
-        <GridItem colStart={{ base: 0, md: 2 }} colEnd={{ base: 1, md: 3 }}>
+        <GridItem colStart={{ base: 0, md: 2 }} colEnd={{ base: 1, md: 5 }}>
           <Text color="gray.500" fontWeight="normal">
             Status Pembayaran
           </Text>
           <Text fontSize="1rem" fontWeight="500">
             {paymentStatus}
           </Text>
+          <Text color="gray.500" fontWeight="normal">
+            No. Resi
+          </Text>
+          <Flex alignItems="center">
+            <Text fontSize="1rem" fontWeight="500" mr="0.5rem">
+              {shippingResi == null ? "-" : shippingResi}
+            </Text>
+            {shippingResi != null && (
+              <IoCopy
+                size="1.25rem"
+                color="#DD6B20"
+                style={{ marginTop: "2px" }}
+                cursor="pointer"
+                onClick={() => {
+                  copyToClipboard(shippingResi, "No. resi berhasil disalin.");
+                  toast({
+                    position: "top",
+                    title: "No. resi berhasil disalin.",
+                    status: "success",
+                    isClosable: true,
+                  });
+                }}
+              />
+            )}
+          </Flex>
         </GridItem>
       </Grid>
       <Divider />
@@ -453,6 +479,7 @@ const PesananSayaDesktop = () => {
                       orderNum={dataPesanan.orders_number}
                       datePurchased={dataPesanan.date_purchased}
                       orderStatus={dataPesanan.orders_status}
+                      shippingResi={dataPesanan.shipping_resi}
                       paymentStatus={dataPesanan.payments_status}
                       dropShip={dataPesanan.dropship_name}
                       deliverer={dataPesanan.delivery_name}
@@ -549,6 +576,7 @@ const PesananSayaMobile = () => {
                     orderNum={dataPesanan.orders_number}
                     datePurchased={dataPesanan.date_purchased}
                     orderStatus={dataPesanan.orders_status}
+                    shippingResi={dataPesanan.shipping_resi}
                     paymentStatus={dataPesanan.payments_status}
                     dropShip={dataPesanan.dropship_name}
                     deliverer={dataPesanan.delivery_name}
