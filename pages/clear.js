194e030e78f69e6bad75bf1ed09e15e331cc0128
ganiilhamirsyadi@gmail.com <ganiilhamirsyadi@gmail.com>
2021-11-15 14:33:48 +0700

    Add redirect to homepage after accessing /clear page

--- pages/clear.js
+++ pages/clear.js
@@ -1,11 +1,16 @@
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 import { deleteAllCookies } from "../utils/cookies";
 
-const ClearData = () => {
+const Clear = () => {
+  const router = useRouter();
   useEffect(() => {
     deleteAllCookies();
     window.localStorage.clear();
+    router.push("/");
   }, []);
   return <p>Data Reset Successfully, try accessing the web again</p>;
 };
+
+export default Clear;
