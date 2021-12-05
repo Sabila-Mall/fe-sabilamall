fb727b4702fa2182af879806e4eca1453b9baa99
Vincent Suryakim <vincentsuryakim@gmail.com>
2021-12-05 22:12:30 +0700

    fixed bug

--- components/Invoice/InvoiceInfoGopay.js
+++ components/Invoice/InvoiceInfoGopay.js
@@ -1,4 +1,4 @@
-import { Text, Flex, Button } from "@chakra-ui/react";
+import { Text, Flex, Box, Button, Image } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 
 import PaymentMethodStepsTabs from "./PaymentMethodStepsTabs";
@@ -7,21 +7,37 @@ export const InvoiceInfoGopay = ({ checkoutResponse }) => {
   const router = useRouter();
 
   const payment_due_date = checkoutResponse?.payment_gateways?.expiry_date;
-  const paymentMethodSteps =
-    checkoutResponse?.payment_gateways?.howtopaypage?.data?.[0]?.step || [];
+  const paymentMethodSteps = checkoutResponse?.payment_gateways?.howtopaypage?.data?.map(
+    (item) => ({
+      title: item.method,
+      steps: item.step,
+    }),
+  );
   const action_url =
     checkoutResponse?.payment_gateways?.merchant_data?.deeplink;
 
   return (
     <>
-      <Text mb="0.5rem">
-        Silahkan melanjutkan pembayaran dengen menekan tombol berikut:
+      <Text fontWeight="700" className="primaryFont" fontSize="1.15rem">
+        QR Code
       </Text>
-      <Flex flexDir="column" w={{ base: "60%", lg: "40%" }} my="1rem">
-        <a href={action_url} target="_blank">
-          <Button colorScheme="orange">Bayar Tagihan</Button>
-        </a>
-      </Flex>
+      <Box w="100%" d="flex" mb="0.6rem" justifyContent="center">
+        <Image
+          src={checkoutResponse?.payment_gateways?.payment_code}
+          alt="qrcode"
+          w="40%"
+        />
+      </Box>
+      <Box mb="0.5rem" d={{ base: "block", lg: "none" }}>
+        <Text className="secondaryFont">
+          Silahkan melanjutkan pembayaran dengen menekan tombol berikut:
+        </Text>
+        <Flex flexDir="column" w={{ base: "60%", lg: "40%" }} my="1rem">
+          <a href={action_url} target="_blank">
+            <Button colorScheme="orange">Bayar Tagihan</Button>
+          </a>
+        </Flex>
+      </Box>
       <Text mb="0.5rem">Lakukan pembayaran sebelum {payment_due_date}</Text>
 
       <Box marginTop={6}>
--- components/Invoice/InvoiceInfoQRIS.js
+++ components/Invoice/InvoiceInfoQRIS.js
@@ -1,12 +1,16 @@
-import { Text, Box, Image, Button } from "@chakra-ui/react";
+import { Text, Box, Flex, Image, Button } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 
 import PaymentMethodStepsTabs from "./PaymentMethodStepsTabs";
 
 export const InvoiceInfoQRIS = ({ checkoutResponse }) => {
   const router = useRouter();
-  const howToPay =
-    checkoutResponse?.payment_gateways?.howtopaypage?.data?.[0]?.step || [];
+  const paymentMethodSteps = checkoutResponse?.payment_gateways?.howtopaypage?.data?.map(
+    (item) => ({
+      title: item.method,
+      steps: item.step,
+    }),
+  );
   return (
     <>
       <Text fontWeight="700" className="primaryFont" fontSize="1.15rem">
@@ -24,7 +28,7 @@ export const InvoiceInfoQRIS = ({ checkoutResponse }) => {
         {/* {checkoutResponse?.payment_gateways?.howtopaypage.name} */}
         Pembayaran dapat dilakukan dengan melakukan langkah-langkah berikut:
       </Text>
-      <PaymentMethodStepsTabs tabsData={howToPay} />
+      <PaymentMethodStepsTabs tabsData={paymentMethodSteps} />
 
       <Flex justifyContent={{ lg: "flex-end" }} w="full">
         <Button
