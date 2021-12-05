fb2a7ef406d5c17b00525faf6d8d86a6659f0552
Vincent Suryakim <vincentsuryakim@gmail.com>
2021-12-05 21:19:48 +0700

    applied review

--- components/Invoice/InvoiceInfoQRIS.js
+++ components/Invoice/InvoiceInfoQRIS.js
@@ -1,10 +1,8 @@
 import { Text, Box, Image, OrderedList, ListItem } from "@chakra-ui/react";
 
-import { CetakTataCaraPembayaranButton } from "../CetakTataCaraPembayaranButton";
-
 export const InvoiceInfoQRIS = ({ checkoutResponse }) => {
   const howToPay =
-    checkoutResponse?.payment_gateways?.howtopaypage?.data?.[0]?.step || "";
+    checkoutResponse?.payment_gateways?.howtopaypage?.data?.[0]?.step || [];
   return (
     <>
       <Text fontWeight="700" className="primaryFont" fontSize="1.15rem">
@@ -17,7 +15,6 @@ export const InvoiceInfoQRIS = ({ checkoutResponse }) => {
           w="40%"
         />
       </Box>
-      <CetakTataCaraPembayaranButton />
       <Text mt="0.6rem" mb="0.4rem" className="secondaryFont">
         {/* {checkoutResponse?.payment_gateways?.howtopaypage.name} */}
         Pembayaran dapat dilakukan dengan melakukan langkah-langkah berikut:
