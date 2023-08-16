82133bd7ba1dc4a12e5179d4ebca7b1da1526a80
Catherine Angel <catheriner40540@gmail.com>
2023-08-16 22:33:49 +0700

    Update jurnal api

--- api/Payment.js
+++ api/Payment.js
@@ -1,34 +1,36 @@
 import axios from "axios";
 
-import { HOST, HOST_3 } from "../constants/api";
+import { HOST, HOST_3, LOCALHOST } from "../constants/api";
 import { getDeviceId } from "../utils/functions";
 
 export const apiGetPaymentMekariPay = async (customerId, orderId) => {
-    const res = await axios.post(`${HOST_3}/api/jurnal/order/payment/link`, {
-        customers_id: customerId,
-        transaction_no: 'SMC' + orderId,
-    });
-    // console.log(res.data);
-    return res.data;
+  const res = await axios.get(
+    `${LOCALHOST}/api/jurnal/order/payment/link?orders_no=SMC${orderId}`,
+  );
+  // console.log(res.data);
+  return res.data;
 };
 
 export const apiConfirmPaymentMekariPay = async (customerId, orderId) => {
-    const res = await axios.post(`${HOST_3}/api/jurnal/order/payment/confirm`, {
-        customers_id: customerId,
-        transaction_no: 'SMC' + orderId,
-    });
-    return res.data;
+  const res = await axios.post(
+    `${LOCALHOST}/api/jurnal/order/payment/confirm`,
+    {
+      orders_no: "SMC" + orderId,
+    },
+  );
+  return res.data;
 };
 
 export const apiCreateVirtualAccount = async (customerId, orderId) => {
-    const res = await axios.post(`${HOST_3}/api/jurnal/order/create_sync`, {
-        customers_id: customerId,
-        transaction_no: 'SMC' + orderId,
-    });
-    return res.data;
-}
+  const res = await axios.post(`${LOCALHOST}/api/jurnal/order/create_sync`, {
+    orders_no: "SMC" + orderId,
+  });
+  return res.data;
+};
 
 export const apiUpdateVirtualAccount = async (customerId, orderId) => {
-    const res = await axios.put(`${HOST_3}/api/jurnal/order/shipping/update/${'SMC' + orderId}`);
-    return res.data;
-}
+  const res = await axios.put(
+    `${LOCALHOST}/api/jurnal/order/shipping/update/${"SMC" + orderId}`,
+  );
+  return res.data;
+};
